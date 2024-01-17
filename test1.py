5d1bf2fb6b4df7aa9d26aff73a796fce58d8fb66
sikicitokat <bbilal60@gmail.com>
2024-01-18 01:10:31 +0300

    second commit

--- test1.py
+++
@@ -1,8 +0,0 @@
-episode_level="1"
-episode_number="1"
-
-episode_level = int(episode_level)
-episode_number = int(episode_number)
-if episode_level in (1, 2) and 1 <= episode_number <= 7:
-    print("düdük")
-
